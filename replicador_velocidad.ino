f69a84f5b3c66e770e80d13194798d6e19371b09
maikollandres <42001250+maikollandres@users.noreply.github.com>
2018-09-11 09:49:41 -0500

    Replicador de velocidad con Interrupción de timer2
    Generador de frecuencia con interrupción del TIMER2 para simular velocidad.
    Utiliza la interrupción del TIMER2 y genera pulsos por el pin 12 de Arduino.
    Tiene listo para simular pulsos para las RPM y el consumo de combustible.
    Esta configurado para una calibración de 1000 pulsos por kilometro.
    La velocidad final que detecta el fm tiene un desfase de 2 km/h a 80 km/ y oscila. Se debe corregir en un futuro

---
+++ replicador_velocidad.ino
@@ -0,0 +1,58 @@
+
+volatile unsigned int cuenta = 0;
+
+bool ESTADO = false;
+//#define Tiempo 216 //Para la base de tiempo (20 kHz)
+#define Tiempo 210 //Ir aumentando a medida que se aumenten las instrucciones de la interrupción--Hacer pruebas con c=0 para que de 20 khz
+
+int a=0; //Para la velocidad
+int b=0; //Para las RPM
+int c=0; //Para el consumo
+
+int i=0;
+double vel=0;
+int param_vel=0;
+double RPM=20;
+int param_rpm=0;
+double litros=50;
+int param_litros=0;
+
+void setup(){ 
+
+   DDRB = B11110000;
+   Serial.begin(9600);  
+   
+   SREG = (SREG & 0b01111111); //Desabilitar interrupciones
+   TIMSK2 = TIMSK2|0b00000001; //Habilita la interrupcion por desbordamiento
+   TCCR2B = 1; //Configura preescala para que FT2 sea de 7812.5Hz
+   SREG = (SREG & 0b01111111) | 0b10000000; //Habilitar interrupciones //Desabilitar interrupciones 
+   TCNT2=Tiempo;
+}
+
+void loop(){  
+  param_vel=(int)20359.96/(10*vel/36)-0.01523;
+  param_rpm=(int)0;
+}
+
+//Interrupción
+ISR(TIMER2_OVF_vect){ 
+      a++;     
+      b++;
+      c++; 
+      if(a>=param_vel) //Contador velocidad
+      {        
+        a=0;        
+        PORTB = PORTB^B0010000;        
+      }   
+      if(b>=1) //Contador RPM
+      {
+        b=0;        
+        PORTB = PORTB^B0100000;        
+      } 
+      if(c>=1) //Contador consumo
+      {
+        c=0;        
+        PORTB = PORTB^B1000000;        
+      }    
+      TCNT2=Tiempo;
+}
